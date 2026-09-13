1c7159b22b5d0a8af6ec31d38943b14dc511f446
Diego <diegofermend@gmail.com>
2026-09-13 13:38:00 +0200

    Aerologistics engine personalized exceptions

---
+++ core/exceptions.py
@@ -0,0 +1,20 @@
+class AerologisticsEngineError(Exception):
+    """Base Aerologistics Engine Exception"""
+    pass
+
+class OverweightLimitError(AerologisticsEngineError):
+    """The total weight is higher than the trucks capacity"""
+    pass
+
+class InvalidPackageError(AerologisticsEngineError):
+    """The dimensions and weight of the packages must be > 0"""
+    pass
+
+class RouteNotFoundError(AerologisticsEngineError):
+    """This vehicle is not registered or has not been found"""
+    pass
+
+class PersistenceError(AerologisticsEngineError):
+    """Error while opening files"""
+    pass    
+
