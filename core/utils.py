638d22474822fd7f09f23d345c5026b52f0af39a
Diego <diegofermend@gmail.com>
2026-09-16 15:49:18 +0200

    audit execution decorator

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,9 @@
 import exceptions as e
 from pathlib import Path
 from datetime import datetime
-from typing import Any
+import time
+from typing import Any, Callable
+from functools import wraps
 
 
 main_p = Path(__file__).resolve().parent.parent
@@ -21,16 +23,47 @@ def inspect_component(obj: Any) -> None:
     print(f"{obj}:\n -Name: {name}\n -Docstring: {doc}\n -Module: {module}\n -Qual Name: {qualname}\n -Dict: {dicti}\n -Slots: {slots}")
 
 
+def get_date() -> str:
+    """Gets the current date and time and returns it as a string"""
+    current_time = datetime.now()
+    current_time = current_time.strftime("%Y/%m/%d %H:%M:%S")
+    return current_time
+
+
 class LoggerMixin:
     """Mixin used to make all child classes inherit the log_event method"""
 
     def log_event(self, message: str) -> None:
         """Logs an event and writes it down onto the app log"""
 
-        current_time = datetime.now()
-
-        current_time = current_time.strftime("%Y/%m/%d %H:%M:%S")
+        times = get_date()
 
         with open(app_log_p, "a") as file1:
-            file1.write(f"[{current_time}] {message}\n")
+            file1.write(f"[{times}] {message}\n")
 
+
+def audit_execution(function: Callable[..., Any]) -> Callable[..., Any]:
+    """Writes the function used and the ms it took on app log"""
+
+    @wraps(function)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        current_date = get_date()
+        initial_time = time.perf_counter()
+
+        try:
+            result = function(*args, **kwargs)
+
+            final_time = time.perf_counter() - initial_time
+
+            with open(app_log_p, "a") as file2:
+                file2.write(f"[{current_date}] SUCCESS: Function {function.__name__} executed in {round(final_time*1000, 3)}ms\n")
+
+            return result
+                
+
+        except Exception as e:
+            with open(app_log_p, "a") as file2:
+                file2.write(f"[{current_date}] ERROR: {e}\n")
+            raise e
+             
+    return wrapper
