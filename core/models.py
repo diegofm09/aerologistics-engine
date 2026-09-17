62f7f0cd1a2eb58c2a6e82154eb31d0c52d3f66d
Diego <diegofermend@gmail.com>
2026-09-17 15:34:58 +0200

    waybill class

---
+++ core/models.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Waybill:
+    """A Waybill class with inmutable atributes"""
+    waybill_id: str
+    sender: str
+    destination: str
+    issued_date: str
+    final_cost: float
+
+    def __post_init__(self):
+        object.__setattr__(self, "waybill_id", self.waybill_id.strip())
+        object.__setattr__(self, "sender", self.sender.strip())
+        object.__setattr__(self, "destination", self.destination.strip())
+        object.__setattr__(self, "issued_date", self.issued_date.strip())
+
+        if self.final_cost <= 0:
+            raise ValueError("The final cost must be higher than 0")
+
+        if self.sender == "":
+            raise ValueError("The Waybill must have a sender")
+
+
