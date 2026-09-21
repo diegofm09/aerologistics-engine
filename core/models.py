d980cfd61c4e8964d035835c0c1d45238181a369
Diego <diegofermend@gmail.com>
2026-09-21 17:58:35 +0200

    Refrigerated package class

--- core/models.py
+++ core/models.py
@@ -149,7 +149,7 @@ class StandardPackage(BasePackage):
 class ExpressPackage(BasePackage):
     """The express package class"""
 
-    def __init__(self, package_id: str, weight_kg: float, destination_zip: str, length: float, width: float, height: float, urgency_level: int):
+    def __init__(self, package_id: str, weight_kg: float, destination_zip: str, length: float, width: float, height: float, urgency_level: int) -> None:
         super().__init__(package_id, weight_kg, destination_zip, length, width, height)
         self.urgency_level = urgency_level
 
@@ -172,36 +172,40 @@ class ExpressPackage(BasePackage):
         return "Express Package"
 
     def __repr__(self):
-        return f"StandardPackage(package_id={self.package_id}, weight_kg={self.weight_kg}, destination_zip={self.destination_zip}, length={self.length}, width={self.width}, height={self.height}, delivered={self.delivered}, urgency_level={self.urgency_level})"
+        return f"ExpressPackage(package_id={self.package_id}, weight_kg={self.weight_kg}, destination_zip={self.destination_zip}, length={self.length}, width={self.width}, height={self.height}, delivered={self.delivered}, urgency_level={self.urgency_level})"
 
     
 class RefrigeratedPackage(BasePackage):
-    def __init__(self, package_id, weight_kg, destination_zip, length, width, height, target_temp_celsius: float):
+    def __init__(self, package_id: str, weight_kg: float, destination_zip: str, length: float, width: float, height: float, target_temp_celsius: float) -> None:
         super().__init__(package_id, weight_kg, destination_zip, length, width, height)
         self.target_temp_celsius = target_temp_celsius
 
     @property
     def target_temp_celsius(self) -> float:
-        return self.target_temp_celsius
+        return self.__target_temp_celsius
 
     @target_temp_celsius.setter
-    def target_temp_celsius(self, new) -> None:
+    def target_temp_celsius(self, new: float) -> None:
         if new<-50.0 or new>5.0:
             raise exceptions.InvalidTemperatureTarget
         else:
             self.__target_temp_celsius = new
 
-    def calculate_volume(self):
-        if self.target_temp_celsius >= -50.0 and self.target_temp_celsius<-30.0:
+    def calculate_volume(self) -> float:
+        if -50.0 <= self.target_temp_celsius <-30.0:
             temp_level = 1.5
-        elif self.target_temp_celsius >= -30.0 and self.target_temp_celsius<-10.0:
+        elif -30.0 <= self.target_temp_celsius <-10.0:
             temp_level = 1.35
-        elif self.target_temp_celsius >= -10.0 and self.target_temp_celsius<0.0:
+        elif -10.0 <= self.target_temp_celsius <0.0:
             temp_level = 1.2
-        elif self.target_temp_celsius >= 0 and self.target_temp_celsius<5.00:
+        elif 0 <= self.target_temp_celsius <= 5.00:
             temp_level = 1.1
         return (self.height*self.width*self.length)*temp_level
 
-    def getr
+    def get_package_type(self) -> str:
+        return "Refrigerated Package"
+
+    def __repr__(self):
+        return f"RefrigeratedPackage(package_id={self.package_id}, weight_kg={self.weight_kg}, destination_zip={self.destination_zip}, length={self.length}, width={self.width}, height={self.height}, delivered={self.delivered}, target_temp_celsius={self.target_temp_celsius})"
 
     
